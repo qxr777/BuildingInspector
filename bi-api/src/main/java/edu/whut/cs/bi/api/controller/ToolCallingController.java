b49182901fb7d8024db7122ac3af97434dc65101
MrZeng <2457270126@qq.com>
2025-06-14 10:37:26 +0800

    feat(api): 添加 ToolCalling 接口实现
    - 新增 ToolCallingController 控制器
    - 实现 getProperty 方法，根据建筑名称查询属性树和相关图片
    - 添加 CauseQuery 类，用于成因分析请求参数

--- bi-api/src/main/java/edu/whut/cs/bi/api/controller/ToolCallingController.java
+++ bi-api/src/main/java/edu/whut/cs/bi/api/controller/ToolCallingController.java
@@ -1,8 +1,62 @@
 package edu.whut.cs.bi.api.controller;
 
+import com.ruoyi.common.core.domain.AjaxResult;
+import edu.whut.cs.bi.api.vo.PropertyTreeVo;
+import edu.whut.cs.bi.biz.controller.FileMapController;
+import edu.whut.cs.bi.biz.domain.Building;
+import edu.whut.cs.bi.biz.domain.FileMap;
+import edu.whut.cs.bi.biz.domain.Property;
+import edu.whut.cs.bi.biz.service.IBuildingService;
+import edu.whut.cs.bi.biz.service.IPropertyService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
- * @author QiXin
+ * @author Junbo
  * @date 2025/6/11
  */
+@Controller
+@RequestMapping("/toolCallingApi")
 public class ToolCallingController {
+    @Resource
+    private FileMapController fileMapController;
+    @Resource
+    private IBuildingService buildingService;
+    @Resource
+    private IPropertyService propertyService;
+
+    @GetMapping("/building/{bName}/property")
+    @ResponseBody
+    public AjaxResult getProperty(@PathVariable("bName") String bName) {
+        if (bName == null) {
+            return AjaxResult.error("参数错误");
+        }
+        Building building_name = new Building();
+        building_name.setName(bName);
+        Long buildingId = buildingService.selectBuildingList(building_name).get(0).getId();
+        Building building = buildingService.selectBuildingById(buildingId);
+        List<FileMap> imageMaps = fileMapController.getImageMaps(buildingId);
+        Map<String, List<String>> collect = imageMaps.stream().collect(Collectors.groupingBy(
+                image -> image.getOldName().split("_")[1],
+                Collectors.mapping(FileMap::getNewName, Collectors.toList())
+        ));
+        Property property = propertyService.selectPropertyTree(building.getRootPropertyId());
+
+        // 封装返回结果
+        PropertyTreeVo propertyTreeVo = new PropertyTreeVo();
+        propertyTreeVo.setProperty(property);
+        propertyTreeVo.setImages(collect);
+        return AjaxResult.success("查询成功", propertyTreeVo);
+    }
+
 }
---
+++ bi-biz/src/main/java/edu/whut/cs/bi/biz/domain/dto/CauseQuery.java
@@ -0,0 +1,30 @@
+package edu.whut.cs.bi.biz.domain.dto;
+
+/**
+ * 成因分析的请求参数
+ */
+public class CauseQuery {
+
+    private String template;
+
+    private String parentObject;
+
+    private String object;
+
+    private String type;
+
+    private String description;
+
+    private String position;
+
+    private String area;
+
+    private Integer level;
+
+    private String componentCode;
+
+    private String componentName;
+
+    private String trend;
+
+}
